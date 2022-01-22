7209ab8e037d3d021297420a2a327545f3892d23
Martin Cho <zoveloper@gmail.com>
2022-01-21 23:53:34 -0500

    WEB301 week1 homework- object with 3properties

--- Week 1- JS Fundmental/homework/js/index.js
+++ Week 1- JS Fundmental/homework/js/index.js
@@ -1,4 +1,6 @@
 const homeWork = (a, b = 2) => {
   alert("Welcome to Seneca College This is a Week " + `${a + b}`);
 };
-homeWork(1);
+homeWork(1); // Methoed
+
+const studnentName = ["martin", "jun", "sang"]; // an object with 3 properties
