2e140226d974f69f07fb11bfec8ba927a9e7d6ea
Martin Cho <zoveloper@gmail.com>
2022-01-26 23:51:25 -0500

    practice objects and functions till  #6

---
+++ Week 2/homework/js/main.js
@@ -0,0 +1,9 @@
+const myFavoritSports = ["soccer", "tennis", "swim"];
+
+const sports = () => {
+  for (i = 0; i < myFavoritSports.length; i++) {
+    console.log("My favorit sports is " + myFavoritSports[i]);
+  }
+};
+
+sports();
