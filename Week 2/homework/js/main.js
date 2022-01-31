f895ff4fec0fab09817de01cb4886138bd8431db
Martin Cho <zoveloper@gmail.com>
2022-01-30 23:57:52 -0500

    practice Objects and Functions

--- Week 2/homework/js/main.js
+++ Week 2/homework/js/main.js
@@ -1,9 +1,24 @@
-const myFavoritSports = ["soccer", "tennis", "swim"];
+const myFavoritSports = [
+  { name: "Martin", sports: "soccer", email: "mc@gmail.com" },
+  { name: "Bruce", sports: "tennis", email: "ba@gmail.com" },
+  { name: "Mark", sports: "swim", email: "ma@gmail.com" },
+];
 
 const sports = () => {
   for (i = 0; i < myFavoritSports.length; i++) {
+    console.log(i);
+    console.log(myFavoritSports.length);
     console.log("My favorit sports is " + myFavoritSports[i]);
   }
 };
+const sports2 = (datas) => {
+  console.log(datas[0]);
+  const sorting = datas.map((data) => {
+    return data.name;
+  });
+  console.log(sorting);
+};
 
-sports();
+console.log(myFavoritSports);
+sports(myFavoritSports);
+sports2(myFavoritSports);
